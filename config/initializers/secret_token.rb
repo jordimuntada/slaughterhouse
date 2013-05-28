e0652ee2e84382655a4da7bebfaf28460e33f4a9
root <root@localhost.localdomain>
2013-05-28 11:46:45 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlaughterHouse::Application.config.secret_token = 'cf138469c6eccb605410ff8044abb2bbd849aef660e2c113462e8ad9b0ff28db6d86e6659ff26dda2236a0551da6b483b589b05d8c02d2836f65f349ebbe283d'
