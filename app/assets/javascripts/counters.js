0f0f4f8bb5ae4d622b4e1e5267f735959214e12f
root <root@localhost.localdomain>
2013-05-29 19:00:25 +0200

    Animal counters mounted up

--- app/assets/javascripts/counters.js
+++ app/assets/javascripts/counters.js
@@ -99,3 +99,43 @@ function timer_tick()
 	document.getElementById("hourstime").innerHTML = hours;
 	document.getElementById("daystime").innerHTML = days;
 }
+
+/*
+ * From now on the animal killings counter is implemented.
+ * The counter proportions are extracted out of http://www.occupyforanimals.org/animal-kill-counter.html
+ */
+
+// Global variables: ___________________________________________________________________
+
+var counts = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+var rate = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+
+// Functions: __________________________________________________________________________
+
+function StartKillCounter() {
+	var millions = [ 90000, 45895, 2262, 1244, 857, 691, 533, 515, 345, 292, 65, 63, 23, 16, 4, 4, 3, 2 ];
+	var perSecond = 8;
+	for (var i = 0; i < counts.length; ++i) 
+		rate[i] = millions[i] * 1000000 / 365 / 24 / 60 / 60 / perSecond;
+	setInterval(NewCounts, 1000 / perSecond);
+}
+
+function NewCounts() {
+	var num, thous, str;
+	for (var i = 0; i < counts.length; ++i) {
+		counts[i] += rate[i];
+		num = Math.round(counts[i]);
+		str = "";
+		while (num > 1000) {
+			thous = num % 1000;
+			if (thous < 10)
+				thous = "00" + thous;
+			else if (thous < 100)
+				thous = "0" + thous;
+			str = "," + thous + str;
+			num = Math.floor(num / 1000);
+		}
+		str = num + str;
+		document.getElementById("count" + i).innerHTML = str;
+	}
+}
